a08bc6a0a248cb57c9e37d2d007d58dbd4e46c8a
sujaybokil <sujaybokil1@gmail.com>
2021-06-14 22:42:39 +0530

    updated config

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,7 @@ alias env-dl="source ~/envs/dl/bin/activate"
 alias env-ga="source ~/envs/ga/bin/activate"
 alias env-base="deactivate"
 
-contains /usr/local/cuda-11.1/bin $fish_user_paths; or set -Ua fish_user_paths /usr/local/cuda-11.1/bin
-contains ~/.local/bin $fish_user_paths; or set -Ua fish_user_paths ~/.local/bin
+fish_add_path /usr/local/cuda-11.1/bin
+fish_add_path ~/.local/bin
 
-neofetch
 
---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,6 @@
+function fish_greeting
+    figlet -t -f slant Heyy Sujay, whatchu doin
+    echo
+    fortune
+    echo
+end
